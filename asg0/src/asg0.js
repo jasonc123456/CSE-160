434be05bcdf3a733d6ab587dbbc34084c5cad16b
Jason Cheung <jasonc123456@corecrafted.net>
2026-01-09 08:20:23 +0000

    removed certain unecessary console.log

--- asg0/src/asg0.js
+++ asg0/src/asg0.js
@@ -32,7 +32,7 @@ function handleDrawEvent(){
   const y1 = parseFloat(document.getElementById('y1Coord').value);
   const x2 = parseFloat(document.getElementById('x2Coord').value);
   const y2 = parseFloat(document.getElementById('y2Coord').value);
-  console.log("x1,y1,x2,y2 =", x1, y1, x2, y2);
+  //console.log("x1,y1,x2,y2 =", x1, y1, x2, y2);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   if (!isNaN(x1) && !isNaN(y1)){
@@ -49,7 +49,7 @@ function handleDrawOperationEvent(){
   const y1 = parseFloat(document.getElementById('y1Coord').value);
   const x2 = parseFloat(document.getElementById('x2Coord').value);
   const y2 = parseFloat(document.getElementById('y2Coord').value);
-  console.log("x1,y1,x2,y2 =", x1, y1, x2, y2);
+  //console.log("x1,y1,x2,y2 =", x1, y1, x2, y2);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) return;
